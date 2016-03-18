af73184a0d3b5d144a1ec268a7de1e7901aeff3c
Daniel Twigg <daniel.twigg@sandicliffe.co.uk>
2016-03-18 14:17:03 +0000

    Added master template

---
+++ resources/views/templates/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('meta-title')</title>
+    <meta name="description" content="@yield('meta-description')">
+</head>
+
+<body>
+
+    @yield('content')
+
+</body>
+</html>
