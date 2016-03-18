23b6edc8f16d1daebc0924fc89f2bb1b95e68d0b
Daniel Twigg <daniel.twigg@sandicliffe.co.uk>
2016-03-18 14:11:08 +0000

    Added DebugBar

--- config/app.php
+++ config/app.php
@@ -157,6 +157,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -203,6 +204,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
